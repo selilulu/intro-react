a3ba1fff8e890f727d1732295d80770c30a32c3e
Selin Tezel <selin.tezel@hotmail.com>
2021-02-16 10:22:22 +0100

    first exercise react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react'
+
+import TodoList from './TodoList';
+import Todo from './Todo';
+
+
 
 function App() {
-  return 
+  //useState returns an array 
+  //todo is=all of our todos setTodos is= function that allows us to update our todos
+  const[todos, setTodos]=useState(['HO','HA','HE'])
+  return (
+    <>
+      <TodoList todoPromps={todos}/>
+      <input type="text" placeholder="Type a new todo"/>
+      <button type="submit">Add Todo</button>
+
+      <h4>Todos</h4>
+      <ul>
+      <li><input type="checkbox"/>  first todo</li>
+      <li><input type="checkbox"/>  second todo</li>
+      </ul>
+      <div> 0 todo left</div>
+  
+
+  </>
+  ) 
+    
+  
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './Todo'
+
+
+export default function TodoList({todoPromps}) {
+    return (
+        <h1>TODO LIST</h1>
+        // {todoPromps.map((todoPromp => {
+        //     return <Todo todoPromp={todoPromp} />
+        // ))}
+    );
+}
